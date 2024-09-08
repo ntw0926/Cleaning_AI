7735f6044d21c5ddf8edbee944114adf1beb6f0e
ntw0926 <38070800+ntw0926@users.noreply.github.com>
2024-09-08 15:51:00 +0900

    base_game setup v1.0
    Finished the Base Game skeleton

--- cleaning_game/cleaning_game.py
+++ cleaning_game/cleaning_game.py
@@ -1,37 +1,171 @@
 import pygame
-from enum import Enum
+from enum import Enum, unique
 from pygame.locals import *
 import numpy as np
-    
-class Roomba():
-    size = pygame.Vector2(3,3)
-    pos = pygame.Vector2(1,1)
+import time
 
-    def __init__(self, size, pos):
-        self.size = size
-        self.pos = pos
+@unique
+class direction(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
 
-    def move(x, y):
-        self.pos.x += x
-        self.pos.y += y
-        
+@unique
+class tile(Enum):
+    Unavailable = 0
+    Dirty = 1
+    Clean = 2
+
+#The Map we are going to clean
 class Map():
-    map_tile = np.zeros((1,1))
-    map_tile.astype(np.int32)
+    map_tile : np.array
 
-    def __init__(self, size:tuple):
-        self.map_tile = np.zeros((size))
+    def __init__(self, array : np.array = np.ones((10,10))):
+        self.map_tile = array
         self.map_tile.astype(np.int32)
 
-    def set_loc_type(self, loc:tuple, tile_type:int):
+    def set_map_tile_type(self, loc:tuple, tile_type:tile):
         try:
-            self.map_tile[loc[0]][loc[1]] = tile_type
+            self.map_tile[loc[0]][loc[1]] = tile_type.value
         except:
             print("Set location " + str(loc) + " with tile type " + str(tile_type) + " failed")
+
     def get_map_tile(self):
         print(self.map_tile)
 
+    def clean_tiles(self, clean_pos: list):
+        for loc in clean_pos:
+            self.set_map_tile_type(loc, tile.Clean)
 
-def base_game():
-    pygame.display.set_caption("Roomba Game")
-    map = Map((10,10))
+    def draw_map(self, surface : pygame.Surface):
+        it = np.nditer(self.map_tile, flags = ['multi_index'])
+        while not it.finished:
+            idx = it.multi_index
+            rect = pygame.Rect(idx[0]*50, idx[1]*50,50,50)
+            color = (255,255,255)
+            if self.map_tile[idx] == tile.Unavailable.value:
+                color = (0,0,0)
+            elif self.map_tile[idx] == tile.Dirty.value:
+                color = (120,120,0)
+            elif self.map_tile[idx] == tile.Clean.value:
+                color = (0, 120, 120)
+            else:
+                print("Unknown type of tile" + str(self.map_tilep[idx]))
+                pygame.quit()
+                break
+            pygame.draw.rect(surface, color, rect)
+            pygame.draw.rect(surface, "black", rect, 1)
+            it.iternext()
+
+
+#Roomba the Vacuum machine
+class Roomba():
+    size : pygame.Vector2
+    #Position of Left bottom 
+    pos : pygame.Vector2
+    arrow = direction.UP
+    
+    def __init__(self, size:pygame.Vector2 = pygame.Vector2(2,2), pos:pygame.Vector2 = pygame.Vector2(0,0)):
+        self.size = size
+        self.pos = pos
+
+    def valid_movement(self, map : Map, movement : pygame.Vector2) -> bool:
+        if movement.x != 0 and movement.y !=0:
+            print("Can't move Diagnoally")
+            return False
+        temp_pos = self.pos + movement
+        clean_arr = []
+        if temp_pos.x < 0 or temp_pos.x + self.size.x > map.map_tile.shape[0]:
+            print("Roomba out of Map")
+            return False
+        if  temp_pos.y < 0 or temp_pos.y + self.size.y > map.map_tile.shape[1]:
+            print("Roomba out of Map")
+            return False
+
+        i = 0
+        for i in range(int(self.size.x)):
+            j=0
+            for j in range(int(self.size.y)):
+                search_tile_x = int(temp_pos.x) + i
+                search_tile_y = int(temp_pos.y) + j
+                if map.map_tile[search_tile_x][search_tile_y] == tile.Unavailable.value:
+                    print("Roomba enter invalid part of Map")
+                    clean_arr.clear()
+                    return False
+                clean_arr.append((search_tile_x, search_tile_y))
+
+        self.pos = temp_pos
+        map.clean_tiles(clean_arr)
+        return True
+        
+
+    def move(self, map : Map, amount : int) -> bool:
+        match self.arrow:
+            case direction.UP:
+                return self.valid_movement(map, pygame.Vector2(0,-1))
+            case direction.RIGHT:
+                return self.valid_movement(map, pygame.Vector2(1,0))
+            case direction.DOWN:
+                return self.valid_movement(map, pygame.Vector2(0,1))
+            case direction.LEFT:
+                return self.valid_movement(map, pygame.Vector2(-1,0))
+            case _:
+                print("Arrow Exception with arrow = " + self.arrow)
+                pygame.quit()
+                return False
+
+    def turn(self, clockwise:bool):
+        val_temp = self.arrow.value
+        if clockwise:
+            val_temp = (val_temp + 1) % 4
+        else :
+            val_temp = (val_temp + 3) % 4
+        self.arrow = direction(val_temp)
+
+    def action(self, map : Map, key_press : direction) -> bool:
+        if self.arrow == key_press:
+            return self.move(map,1)
+        elif (self.arrow.value + 1)%4 == key_press.value:
+            self.turn(True)
+            return True
+        elif (self.arrow.value + 3)%4 == key_press.value:
+            self.turn(False)
+            return True
+        else:
+            print("invalid movement")
+            return False
+
+    def get_info(self):
+        print("Roomba at " + str(self.pos))
+        print("Roomba facing " + str(self.arrow))
+
+    def draw_Roomba(self, surface:pygame.Surface):
+        rect = pygame.Rect(int(self.pos[0])*50, int(self.pos[1])*50, int(self.size[0])*50, int(self.size[1])*50)
+        pygame.draw.rect(surface, (100,100,100) , rect, 0, 15)
+        triangle = [[0,0],[0,0],[0,0]]
+
+        if self.arrow == direction.UP:
+            top_mid = [self.pos[0]*50+self.size[0]*25 , self.pos[1]*50 + 10]
+            left = [top_mid[0] - 10, top_mid[1] + 10]
+            right = [top_mid[0] + 10, top_mid[1] + 10]
+            triangle = [top_mid, left, right]
+
+        elif self.arrow == direction.RIGHT:
+            right_mid = [self.pos[0]*50 + self.size[0]*50 - 10 , self.pos[1]*50 + self.size[1] * 25]
+            top = [right_mid[0] - 10, right_mid[1] - 10]
+            bottom = [right_mid[0] - 10, right_mid[1] + 10]
+            triangle = [right_mid, top, bottom]
+
+        elif self.arrow == direction.DOWN:
+            bottom_mid = [self.pos[0]*50+self.size[0]*25 , self.pos[1]*50 + self.size[1]*50 - 10]
+            left = [bottom_mid[0] - 10, bottom_mid[1] - 10]
+            right = [bottom_mid[0] + 10, bottom_mid[1] - 10]
+            triangle = [bottom_mid, left, right]
+
+        elif self.arrow == direction.LEFT:
+            left_mid = [self.pos[0]*50+10 , self.pos[1]*50 + self.size[1] * 25]
+            top = [left_mid[0] + 10, left_mid[1] - 10]
+            bottom = [left_mid[0] + 10, left_mid[1] + 10]
+            triangle = [left_mid, top, bottom]
+        pygame.draw.polygon(surface, "red", triangle, 0)
--- cleaning_game/game_run.py
+++ cleaning_game/game_run.py
@@ -1,25 +1,52 @@
 import pygame
 from pygame.locals import *
 from cleaning_game import *
+import pre_defined_maps
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1280,720))
+    screen = pygame.display.set_mode((720,720))
+    screen.fill("white")
     clock = pygame.time.Clock()
     running = True
-
+    
+    pygame.display.set_caption("Roomba Game")
+    map = Map(pre_defined_maps.map_with_table)
+    roomba = Roomba()
+    roomba.valid_movement(map, pygame.Vector2(0,0))
+    map.draw_map(screen)
+    roomba.draw_Roomba(screen)
+    pygame.display.flip()
     while running:
+        update = False
         for event in pygame.event.get():
-           if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 running = False
-        base_game()
-        screen.fill("purple")
-        pygame.display.flip()
+            elif event.type == pygame.KEYUP:
 
+                if event.key == pygame.K_UP:
+                    update = roomba.action(map, direction.UP)
+                    roomba.get_info()
+
+                elif event.key == pygame.K_RIGHT:
+                    update = roomba.action(map, direction.RIGHT)
+                    roomba.get_info()
+
+                elif event.key == pygame.K_DOWN:
+                    update = roomba.action(map, direction.DOWN)
+                    roomba.get_info()
+
+                elif event.key == pygame.K_LEFT:
+                    update = roomba.action(map, direction.LEFT)
+                    roomba.get_info()
+        if update:
+            map.draw_map(screen)
+            roomba.draw_Roomba(screen)
+            pygame.display.flip()
         clock.tick(60)
 
-
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
